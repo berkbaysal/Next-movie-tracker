7658cf977860808a620b55d416cff9030bf082e0
berkbaysal <berkbaysal@gmail.com>
2022-09-01 15:09:45 +0300

    bug fixes for deployment

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,14 @@
-
-import React from 'react'
+import React from 'react';
 import Layout from '../components/Layout';
 
 const Home = () => {
+  console.log('url:');
+  console.log(process.env.VERCEL_URL);
   return (
     <Layout>
       <div>Home</div>
     </Layout>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
